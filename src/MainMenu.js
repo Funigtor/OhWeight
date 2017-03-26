b2072eea25b5092504414473cec9bd7acf2dbfed
Aurélien Chevalier <funigtor@me.com>
2017-03-26 14:11:32 +0200

    Utilisation de la touche Entrée en plus de la touche Espace pour démarrer le jeu.

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,15 +1,17 @@
 Bouffe.MainMenu = function(game) {
 	var keyStart;
+	var keySelect;
 	this.create = function() {
 		this.add.sprite(0, 0, 'screen-mainmenu');
 		this.startButton = this.add.button(Bouffe._WIDTH*0.75, Bouffe._HEIGHT*0.66, 'button-start', this.startGame, this, 2, 0, 1);
 		this.startButton.anchor.set(0.5,0);
 		this.startButton.input.useHandCursor = true;
-		keyStart = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		keyStart = this.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+		keySelect = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	};
 
 	this.update = function() {
-		if (keyStart.isDown) this.startGame();
+		if (keyStart.isDown || keySelect.isDown) this.startGame();
 	};
 
 	this.startGame = function() {
