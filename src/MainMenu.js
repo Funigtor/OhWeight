bc9e82c0b09c5cf73c7d3e530d18c3441043e0e5
Aurélien Chevalier <funigtor@me.com>
2017-03-27 09:57:42 +0200

    Ajout d'un bouton pour changer le personnage.

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,6 +1,9 @@
 Bouffe.MainMenu = function(game) {
 	var keyStart;
 	var keySelect;
+	var userData = new Object();
+	userData.personnage='dude';
+
 	this.create = function() {
 		this.add.sprite(0, 0, 'screen-mainmenu');
 		this.startButton = this.add.button(Bouffe._WIDTH*0.75, Bouffe._HEIGHT*0.66, 'button-start', this.startGame, this, 2, 0, 1);
@@ -8,6 +11,9 @@ Bouffe.MainMenu = function(game) {
 		this.startButton.input.useHandCursor = true;
 		keyStart = this.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		keySelect = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.persoButton = this.add.button(Bouffe._WIDTH*0.80, Bouffe._HEIGHT*0.45, 'button-dude', this.changePerso, this, 2, 0, 1);
+		this.persoButton.anchor.set(0.5,0);
+		this.persoButton.input.useHandCursor = true;
 	};
 
 	this.update = function() {
@@ -15,7 +21,13 @@ Bouffe.MainMenu = function(game) {
 	};
 
 	this.startGame = function() {
-		this.game.state.start('Game');
+		this.game.state.start('Game',false,false,userData);
 	};
 
+	this.changePerso = function(){
+		(userData.personnage == "dude") ? userData.personnage = "dudette" : userData.personnage = "dude"
+		let boutonACharger = 'button-'+userData.personnage;
+		this.persoButton = this.add.button(Bouffe._WIDTH*0.80, Bouffe._HEIGHT*0.45, boutonACharger, this.changePerso, this, 2, 0, 1);
+		this.persoButton.anchor.set(0.5,0);
+	};
 };
--- src/Preloader.js
+++ src/Preloader.js
@@ -8,7 +8,10 @@ Bouffe.Preloader.prototype = {
             this.load.image('saucisson', 'assets/saucisson.png',243,207);
 			this.load.image('screen-mainmenu', 'assets/MainMenu.png');
 			this.load.spritesheet('button-start', 'assets/button-start.png', 146, 51);
+			this.load.spritesheet('button-dude', 'assets/boutondude.png', 100, 100);
+			this.load.spritesheet('button-dudette', 'assets/boutondudette.png', 100, 100);
             this.load.spritesheet('dude', 'assets/dude.png', 32, 36);
+			this.load.spritesheet('dudette', 'assets/dudette.png', 32, 48);
 			this.load.json('level-one', 'levels/lvl1.json');
 	},
 	create: function() {
